85da978075b139648227703aaf35585ab4c3dd56
KumarReddy409 <kumarreddy505@gmail.com>
2020-04-18 21:25:52 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
+
+
+
+ const routes: Routes = [
+      {
+        path:'register',
+       
+        component:RegisterComponent
+      },
+      {
+        path:"login",component:LoginComponent
+      }
+
+  
+ ]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
